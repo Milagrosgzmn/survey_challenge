756ce57e69f5b7b2a8e3aaab301dde5ca61c92d9
milagros <miliguzman0@gmail.com>
2023-09-29 05:40:32 -0300

    database

--- server/DB_connection.js
+++ server/DB_connection.js
@@ -1,12 +1,17 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
-
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORTDB, DB_URL } = process.env;
+const PORT = DB_PORTDB || 5432;
 const ResponsesModel = require('./models/responses');
-
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/survey`,
-   { logging: false, native: false }
-);
+const conection = DB_URL || {
+    dialect: 'postgres',
+  host: DB_HOST,
+  port: PORT, 
+  username: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+}
+const sequelize = new Sequelize(conection);
 
 ResponsesModel(sequelize);
 
