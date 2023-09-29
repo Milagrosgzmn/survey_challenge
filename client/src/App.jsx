4049d82d1bd9dc53fd6ad3d449ce85061eef1bca
milagros <miliguzman0@gmail.com>
2023-09-29 04:35:30 -0300

    added(client/app)addedStyles&Imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { Routes, Route, useLocation } from 'react-router-dom';
 
 import Home from './views/home/home';
@@ -5,17 +6,23 @@ import Consult from './views/consult/consult';
 import Error from './views/error/error';
 import Survey from './views/survey/survey';
 import UpdateSurvey from './views/updateSurvey/updateSurvey';
+import Success from "./views/success/success";
 
 import NavBar from './components/navBar/navBar';
 
 import './App.css';
 
+const apiBackUrl = import.meta.env.VITE_API_BACK_URL;
+const urlApi = apiBackUrl || 'http://localhost:3001';
+axios.defaults.baseURL = urlApi;
+
 function App() {
   let location = useLocation();
-  const allowedPaths = ['/', '/survey', '/consult'];
+  const allowedPaths = ['/', '/survey','/survey/success', "/survey/update",'/consult'];
 
   return (
     <>
+    
       {allowedPaths.includes(location.pathname) && <NavBar />}
       <Routes>
         <Route path='/' element={<Home/>} />
@@ -23,6 +30,7 @@ function App() {
         <Route path='/consult' element={<Consult/>} />
         <Route path='/survey/update' element={<UpdateSurvey/>} />
         <Route path='*' element={<Error/>} />
+        <Route path='/survey/success' element={<Success/>} />
       </Routes>  
     </>
   );
